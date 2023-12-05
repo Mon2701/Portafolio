61dff8c48f212746f00565bfd203ae378e406ea8
Monse <matciry@gmail.com>
2023-12-04 19:37:29 -0600

    fisrt commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./sw.js')
+    .then(registrado=> console.log("el service worker se registro",
+    registrado))
+    .catch(error=>console.log("error al registrar el service worker",
+    error));
+}else{ 
+    console.log("El service worker no es compatible")
+}
---
+++ sw.js
@@ -0,0 +1,55 @@
+const nombreCache = "PortAM";
+const archivosCache = [
+    "/",
+    "index.html",
+    "css/style.css",
+    "js/app.js",
+    "js/script.js"
+]
+
+self.addEventListener('install', e => {
+    console.log("El SW se instaló", e);
+    e.waitUntil(
+        caches.open(nombreCache)
+        .then((cache) => {
+            console.log();
+            return cache.addAll(archivosCache);
+        })
+        .catch(error => {
+            console.error('Error durante la instalación del caché:', error);
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    console.log("Fetch...", e);
+    e.respondWith(
+        caches.match(e.request)
+        .then(respuestaCache => {
+            return respuestaCache || fetch(e.request)
+                .then(respuestaRed => {
+                    return caches.open(nombreCache)
+                        .then(cache => {
+                            cache.put(e.request, respuestaRed.clone());
+                            return respuestaRed;
+                        });
+                })
+                .catch(error => {
+                    console.error('Error al intentar recuperar el recurso:', error);
+                });
+        })
+    );
+});
+
+self.addEventListener('activate', e => {
+    console.log("El SW está activo", e);
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => {
+                if (key !== nombreCache) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
